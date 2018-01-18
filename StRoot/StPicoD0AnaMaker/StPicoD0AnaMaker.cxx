ffed98835a9bc5a2331c53d9ba7f1de48f4b869a
luicasko <l.kramarik@hotmail.sk>
2018-01-18 15:05:25 +0100

    basic QA added

--- StRoot/StPicoD0AnaMaker/StPicoD0AnaMaker.cxx
+++ StRoot/StPicoD0AnaMaker/StPicoD0AnaMaker.cxx
@@ -28,6 +28,32 @@ int StPicoD0AnaMaker::InitHF() {
     //
     // EXAMPLE //  mOutList->Add(new TH1F(...));
     // EXAMPLE //  TH1F* hist = static_cast<TH1F*>(mOutList->Last());
+    mOutList->Add(new TH2F("h_piTOF","h_piTOF",100,0,10, 250, -1, 1.5));
+    mOutList->Add(new TH2F("h_kTOF","h_kTOF",100,0,10, 250, -1, 1.5));
+    mOutList->Add(new TH2F("h_pTOF","h_pTOF",100,0,10, 250, -1, 1.5));
+
+    mOutList->Add(new TH2F("h_piTOF_20","h_piTOF_20",100,0,10, 100, 0, 1));
+    mOutList->Add(new TH2F("h_kTOF_20","h_kTOF_20",100,0,10, 100, 0, 1));
+    mOutList->Add(new TH2F("h_pTOF_20","h_pTOF_20",100,0,10, 100, 0, 1));
+
+    mOutList->Add(new TH2F("h_piTOF_HFT","h_piTOF_HFT",100,0,10, 100, 0, 1));
+    mOutList->Add(new TH2F("h_kTOF_HFT","h_kTOF_HFT",100,0,10, 100, 0, 1));
+    mOutList->Add(new TH2F("h_pTOF_HFT","h_pTOF_HFT",100,0,10, 100, 0, 1));
+
+    mOutList->Add(new TH2F("h_piTOF_HFT_20","h_piTOF_HFT_20",100,0,10, 100, 0, 1));
+    mOutList->Add(new TH2F("h_kTOF_HFT_20","h_kTOF_HFT_20",100,0,10, 100, 0, 1));
+    mOutList->Add(new TH2F("h_pTOF_HFT_20","h_pTOF_HFT_20",100,0,10, 100, 0, 1));
+
+    mOutList->Add(new TH2F("h_piTOFbeta","h_piTOFbeta",500,0,10, 100, 0, 1));
+    mOutList->Add(new TH2F("h_kTOFbeta","h_kTOFbeta",500,0,10, 100, 0, 1));
+    mOutList->Add(new TH2F("h_pTOFbeta","h_pTOFbeta",500,0,10, 100, 0, 1));
+
+    mOutList->Add(new TH2F("h_pinsigma","h_pinsigma",1000,0,10, 99, -5, 5));
+    mOutList->Add(new TH2F("h_knsigma","h_knsigma",1000,0,10, 99, -5, 5));
+    mOutList->Add(new TH2F("h_pnsigma","h_pnsigma",1000,0,10, 99, -5, 5));
+
+    mOutList->Add(new TH2F("h_dedx","h_dedx", 1000, 0, 10, 1000, 0, 10));
+
     mOutFileBaseName = mOutFileBaseName.ReplaceAll(".root", "");
 
     ntp_DMeson_Signal = new TNtuple("ntp_signal","DMeson TreeSignal","grefMult:pi1_runId:pi1_eventId:pi1_phi:pi1_eta:pi1_pt:pi1_dca:pi1_dedx:pi1_nSigma:pi1_nHitFit:pi1_nHitdedx:pi1_TOFinvbeta:pi1_betaBase:k_runId:k_eventId:k_phi:k_eta:k_pt:k_dca:k_dedx:k_nSigma:k_nHitFit:k_nHitdedx:k_TOFinvbeta:k_betaBase:dcaDaughters:flag:primVz:D_theta:cosTheta:D_decayL:dcaD0ToPv:D_phi:D_eta:D_cosThetaStar:D_pt:D_mass:D_mass_LS:D_mass_US");
@@ -53,19 +79,87 @@ int StPicoD0AnaMaker::FinishHF() {
 //    mOutFile->Close();
     return kStOK;
 }
-
 // _________________________________________________________
 int StPicoD0AnaMaker::MakeHF() {
-    // -- process event
-    //    ADD YOUR PROCESSING CODE HERE
-    //    ... it is usefull to use the methods below
-    //     - createCandidates()
-    //     - analyzeCandidates()
-        //cout<<"going to create candidates"<<endl;
-        createCandidates();
-        //cout<<"candidated created, going to analyze them"<<endl;
+    createCandidates();
 //        analyzeCandidates();
-        //cout<<"candidates analysed"<<endl;
+    TH2F *h_piTOF = static_cast<TH2F*>(mOutList->FindObject("h_piTOF"));
+    TH2F *h_kTOF = static_cast<TH2F*>(mOutList->FindObject("h_kTOF"));
+    TH2F *h_pTOF = static_cast<TH2F*>(mOutList->FindObject("h_pTOF"));
+
+    TH2F *h_piTOF_20 = static_cast<TH2F*>(mOutList->FindObject("h_piTOF_20"));
+    TH2F *h_kTOF_20 = static_cast<TH2F*>(mOutList->FindObject("h_kTOF_20"));
+    TH2F *h_pTOF_20 = static_cast<TH2F*>(mOutList->FindObject("h_pTOF_20"));
+
+    TH2F *h_piTOF_HFT = static_cast<TH2F*>(mOutList->FindObject("h_piTOF_HFT"));
+    TH2F *h_kTOF_HFT = static_cast<TH2F*>(mOutList->FindObject("h_kTOF_HFT"));
+    TH2F *h_pTOF_HFT = static_cast<TH2F*>(mOutList->FindObject("h_pTOF_HFT"));
+
+    TH2F *h_piTOF_HFT_20 = static_cast<TH2F*>(mOutList->FindObject("h_piTOF_HFT_20"));
+    TH2F *h_kTOF_HFT_20 = static_cast<TH2F*>(mOutList->FindObject("h_kTOF_HFT_20"));
+    TH2F *h_pTOF_HFT_20 = static_cast<TH2F*>(mOutList->FindObject("h_pTOF_HFT_20"));
+
+    TH2F *h_piTOFbeta = static_cast<TH2F*>(mOutList->FindObject("h_piTOFbeta"));
+    TH2F *h_kTOFbeta = static_cast<TH2F*>(mOutList->FindObject("h_kTOFbeta"));
+    TH2F *h_pTOFbeta = static_cast<TH2F*>(mOutList->FindObject("h_pTOFbeta"));
+
+    TH2F *h_pinsigma = static_cast<TH2F*>(mOutList->FindObject("h_pinsigma"));
+    TH2F *h_knsigma = static_cast<TH2F*>(mOutList->FindObject("h_knsigma"));
+    TH2F *h_pnsigma = static_cast<TH2F*>(mOutList->FindObject("h_pnsigma"));
+
+    TH2F *h_dedx = static_cast<TH2F*>(mOutList->FindObject("h_dedx"));
+
+    StThreeVectorF pVtx = mPicoDst->event()->primaryVertex();
+
+    UInt_t nTracks = mPicoDst->numberOfTracks();
+    for (unsigned short iTrack = 0; iTrack < nTracks; ++iTrack){
+        StPicoTrack const* trk = mPicoDst->track(iTrack);
+        if (!trk) continue;
+        StPhysicalHelixD helix = trk->helix(mBField);
+        StThreeVectorF momentum = trk->gMom(pVtx, mPicoDst->event()->bField());
+
+        if (!(trk->nHitsFit()>=15)) continue;
+        if (!(fabs(momentum.pseudoRapidity()) <= 1.0)) continue;
+
+        if (fabs(trk->nSigmaPion())<3.0){
+            if (mHFCuts->isTOFHadron(trk, mHFCuts->getTofBetaBase(trk), StHFCuts::kPion)) {
+                    h_piTOF->Fill(trk->gPt(),mHFCuts->getTofBetaBase(trk));
+                    float oneOverBeta = getOneOverBeta(trk, mHFCuts->getTofBetaBase(trk), StHFCuts::kPion);
+                    h_piTOFbeta->Fill(trk->gPt(),oneOverBeta);
+                    if (trk->nHitsFit()>=20) h_piTOF_20->Fill(trk->gPt(),oneOverBeta);
+                    if (trk->isHFTTrack()) h_piTOF_HFT->Fill(trk->gPt(),oneOverBeta);
+                    if ((trk->isHFTTrack()) && (trk->nHitsFit()>=20)) h_piTOF_HFT_20->Fill(trk->gPt(),oneOverBeta);
+            }
+        }
+
+        if (fabs(trk->nSigmaKaon())<3.0){
+            if (mHFCuts->isTOFHadron(trk, mHFCuts->getTofBetaBase(trk), StHFCuts::kKaon)) {
+                h_kTOF->Fill(trk->gPt(),mHFCuts->getTofBetaBase(trk));
+                float oneOverBeta = getOneOverBeta(trk, mHFCuts->getTofBetaBase(trk), StHFCuts::kKaon);
+                h_kTOFbeta->Fill(trk->gPt(),oneOverBeta);
+                if (trk->nHitsFit()>=20) h_kTOF_20->Fill(trk->gPt(),oneOverBeta);
+                if (trk->isHFTTrack()) h_kTOF_HFT->Fill(trk->gPt(),oneOverBeta);
+                if ((trk->isHFTTrack()) && (trk->nHitsFit()>=20)) h_kTOF_HFT_20->Fill(trk->gPt(),oneOverBeta);
+            }
+        }
+
+        if (fabs(trk->nSigmaProton())<3.0){
+            if (mHFCuts->isTOFHadron(trk, mHFCuts->getTofBetaBase(trk), StHFCuts::kProton)) {
+                h_pTOF->Fill(trk->gPt(),mHFCuts->getTofBetaBase(trk));
+                float oneOverBeta = getOneOverBeta(trk, mHFCuts->getTofBetaBase(trk), StHFCuts::kProton);
+                h_pTOFbeta->Fill(trk->gPt(),oneOverBeta);
+                if (trk->nHitsFit()>=20) h_pTOF_20->Fill(trk->gPt(),oneOverBeta);
+                if (trk->isHFTTrack()) h_pTOF_HFT->Fill(trk->gPt(),oneOverBeta);
+                if ((trk->isHFTTrack()) && (trk->nHitsFit()>=20)) h_pTOF_HFT_20->Fill(trk->gPt(),oneOverBeta);
+            }
+        }
+
+        h_pinsigma->Fill(momentum.mag(),trk->nSigmaPion());
+        h_knsigma->Fill(momentum.mag(),trk->nSigmaKaon());
+        h_pnsigma->Fill(momentum.mag(),trk->nSigmaProton());
+        h_dedx->Fill(momentum.mag(),trk->dEdx());
+
+    } // .. end tracks loop
 
    return kStOK;
 }
@@ -77,12 +171,9 @@ int StPicoD0AnaMaker::createCandidates() {
         for (unsigned short idxKaon = 0; idxKaon < mIdxPicoKaons.size(); ++idxKaon) {
             StPicoTrack const *kaon = mPicoDst->track(mIdxPicoKaons[idxKaon]);
             if (mIdxPicoKaons[idxKaon] == mIdxPicoPions[idxPion1]) continue;
-            // -- Making pair
             StHFPair *pair = new StHFPair(pion1, kaon, mHFCuts->getHypotheticalMass(StHFCuts::kPion),mHFCuts->getHypotheticalMass(StHFCuts::kKaon), mIdxPicoPions[idxPion1],mIdxPicoKaons[idxKaon], mPrimVtx, mBField, kTRUE);
-//            StHFPair pair(pion1, kaon, mHFCuts->getHypotheticalMass(StHFCuts::kPion),mHFCuts->getHypotheticalMass(StHFCuts::kKaon), mIdxPicoPions[idxPion1],mIdxPicoKaons[idxKaon], mPrimVtx, mBField, kTRUE);
-            if (!mHFCuts->isClosePair(pair)) continue;
-//            mPicoHFEvent->addHFSecondaryVertexPair(&pair);
 
+            if (!mHFCuts->isClosePair(pair)) continue;
 //            Filling ntp
             if(pair->pt() < 1) continue;
             if(pair->pt() > 2) continue;
