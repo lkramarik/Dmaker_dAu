326114f21575c6ca2a0cd6eb8e1e676361d759ee
luicasko <l.kramarik@hotmail.sk>
2018-02-01 01:02:28 +0100

    should work now

--- StRoot/StPicoHFMaker/StPicoHFMaker.cxx
+++ StRoot/StPicoHFMaker/StPicoHFMaker.cxx
@@ -58,7 +58,7 @@ Int_t StPicoHFMaker::Init() {
 
   // -- file which holds list of histograms
   mOutputFileList = new TFile(Form("%s.%s.root", mOutputFileBaseName.Data(), GetName()), "RECREATE");
-//  mOutputFileList->SetCompressionLevel(1);
+  mOutputFileList->SetCompressionLevel(1);
 
   // -- disable automatic adding of objects to file
   bool oldStatus = TH1::AddDirectoryStatus();
