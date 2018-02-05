b7506080c75dbbeddecc02ecf096ec392db548a4
luicasko <l.kramarik@hotmail.sk>
2018-02-05 23:52:01 +0100

    another test

--- StRoot/StPicoCutsBase/StPicoCutsBase.cxx
+++ StRoot/StPicoCutsBase/StPicoCutsBase.cxx
@@ -247,7 +247,7 @@ bool StPicoCutsBase::isTPCHadron(StPicoTrack const * const trk, int pidFlag) con
   else if (pidFlag == kProton)
     nSigma = fabs(trk->nSigmaProton());
 
-  return (nSigma < 3);
+  return (nSigma < 2);
 }
 
 bool StPicoCutsBase::isTOFHadronPID(StPicoTrack const *trk, float const & tofBeta, int pidFlag) const {
