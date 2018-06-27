b76bc2b2a2ca31bfee5d3be91ae5a5086dc00232
luicasko <l.kramarik@hotmail.sk>
2018-06-27 15:27:08 +0200

    nest

--- StRoot/StPicoMixedEventMaker/StMixerPair.cxx
+++ StRoot/StPicoMixedEventMaker/StMixerPair.cxx
@@ -43,8 +43,8 @@ StMixerPair::StMixerPair(StPicoTrack const  particle1, StPicoTrack const particl
 
     StThreeVectorF dVtx = vtx1 -vtx2;
 
-    StPhysicalHelixD p1Helix(particle1 -> gMom(), particle1 -> origin(),bField*kilogauss, particle1 -> charge());
-    StPhysicalHelixD p2Helix(particle2 -> gMom(), particle2 -> origin() + dVtx, bField*kilogauss,  particle2 -> charge());
+    StPhysicalHelixD p1Helix(particle1.gMom(), particle1.origin(),bField*kilogauss, particle1.charge());
+    StPhysicalHelixD p2Helix(particle2.gMom(), particle2.origin() + dVtx, bField*kilogauss,  particle2.charge());
 
     // -- move origins of helices to the primary vertex origin
     p1Helix.moveOrigin(p1Helix.pathLength(vtx1));
@@ -54,8 +54,8 @@ StMixerPair::StMixerPair(StPicoTrack const  particle1, StPicoTrack const particl
     StThreeVectorF const p1Mom = p1Helix.momentum(bField * kilogauss);
     StThreeVectorF const p2Mom = p2Helix.momentum(bField * kilogauss);
 
-    StPhysicalHelixD const p1StraightLine(p1Mom, p1Helix.origin(), 0, particle1 -> charge());
-    StPhysicalHelixD const p2StraightLine(p2Mom, p2Helix.origin(), 0, particle2 -> charge());
+    StPhysicalHelixD const p1StraightLine(p1Mom, p1Helix.origin(), 0, particle1.charge());
+    StPhysicalHelixD const p2StraightLine(p2Mom, p2Helix.origin(), 0, particle2.charge());
 
     pair<double, double> const ss = p1StraightLine.pathLengths(p2StraightLine);
     StThreeVectorF const p1AtDcaToP2 = p1StraightLine.at(ss.first);
