9dda7670bce4312843fa6f9d00818c41f91cd280
luicasko <l.kramarik@hotmail.sk>
2018-06-28 10:52:22 +0200

    run file

--- StRoot/macros/runPicoMixedEvent.C
+++ StRoot/macros/runPicoMixedEvent.C
@@ -84,7 +84,7 @@ void runPicoMixedEvent(
 
   StPicoDstMaker* picoDstMaker = new StPicoDstMaker(static_cast<StPicoDstMaker::PicoIoMode>(StPicoDstMaker::IoRead), inputFile, "picoDstMaker");
   cout<<"ok, picoDstMaker created"<<endl;
-  StPicoMixedEventMaker* picoMixedEventMaker = new StPicoMixedEventMaker("picoMixedEventMaker", picoDstMaker, hfCuts, outputFile, "");
+  StPicoMixedEventMaker* picoMixedEventMaker = new StPicoMixedEventMaker("picoMixedEventMaker", picoDstMaker, hfCuts, outputFile, inputFile);
 
   clock_t start = clock(); // getting starting time
   chain->Init();
