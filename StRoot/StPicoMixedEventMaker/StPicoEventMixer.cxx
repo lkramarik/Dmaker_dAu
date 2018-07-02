fe92198b9bc3c5a2071116ef9de3afcc2d476605
luicasko <l.kramarik@hotmail.sk>
2018-07-02 11:00:43 +0200

    next

--- StRoot/StPicoMixedEventMaker/StPicoEventMixer.cxx
+++ StRoot/StPicoMixedEventMaker/StPicoEventMixer.cxx
@@ -69,12 +69,13 @@ bool StPicoEventMixer::addPicoEvent(StPicoDst const* const picoDst, float weight
         StPicoTrack const* trk = picoDst->track(iTrk);
         cout<<"StPicoTrack const* trk = picoDst->track(iTrk);"<<endl;
         if (!trk) continue;
+
 //        if(!mHFCuts->isGoodTrack(trk)) {
 //            cout<<"not good track"<<endl;
 //            continue;
 //        }
         cout<<"if(!mHFCuts->isGoodTrack(trk))"<<endl;
-        const float beta = mHFCuts->getTofBetaBase(trk);
+//        const float beta = mHFCuts->getTofBetaBase(trk);
         cout<<"const float beta = mHFCuts->getTofBetaBase(trk);"<<endl;
         bool saveTrack = false;
         int pidFlag = StPicoCutsBase::kPion;
