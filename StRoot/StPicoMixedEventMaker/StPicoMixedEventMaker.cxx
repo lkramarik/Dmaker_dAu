987cf2db791b81b037e3bea9a04219ce0ec602d2
luicasko <l.kramarik@hotmail.sk>
2018-07-11 16:14:51 +0200

    one centrality bin

--- StRoot/StPicoMixedEventMaker/StPicoMixedEventMaker.cxx
+++ StRoot/StPicoMixedEventMaker/StPicoMixedEventMaker.cxx
@@ -20,8 +20,11 @@
 
 ClassImp(StPicoMixedEventMaker)
 
-static const int m_nmultEdge = 7;
-static float constexpr m_multEdge[m_nmultEdge+1] = {0, 4, 8, 12, 16, 20, 24, 200};
+//static const int m_nmultEdge = 7;
+//static float constexpr m_multEdge[m_nmultEdge+1] = {0, 4, 8, 12, 16, 20, 24, 200};
+
+static const int m_nmultEdge = 1;
+static float constexpr m_multEdge[m_nmultEdge+1] = {0, 2000};
 
 // _________________________________________________________
 StPicoMixedEventMaker::StPicoMixedEventMaker(char const* name, StPicoDstMaker* picoMaker, StHFCuts* hfCuts,
