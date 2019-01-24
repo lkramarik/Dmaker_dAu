4e52676abea13994a176babcf009f1a75ffbe568
luicasko <l.kramarik@hotmail.sk>
2019-01-24 15:43:10 +0100

    PV refit

--- StRoot/StPicoKFVertexFitter/StPicoKFVertexFitter.cxx
+++ StRoot/StPicoKFVertexFitter/StPicoKFVertexFitter.cxx
@@ -28,7 +28,7 @@ TVector3 StPicoKFVertexFitter::primaryVertexRefit(StPicoDst const* const picoDst
         StPicoTrack* gTrack = (StPicoTrack*)picoDst->track(iTrk);
         if (! gTrack) continue;
         if(std::binary_search(tracksToRemove.begin(), tracksToRemove.end(), iTrk)) continue;
-//        if(!gTrack->isPrimary()) continue;
+        if(!gTrack->isPrimary()) continue;
 //        cout<<gTrack->gDCAz(Vtx.z())<<endl;
         if(abs(gTrack->gDCAz(Vtx.z()))>3) continue;
         if(abs(gTrack->gDCAxy(Vtx.x(),Vtx.y()))>1.5) continue;
@@ -70,9 +70,8 @@ TVector3 StPicoKFVertexFitter::primaryVertexRefitUsingTracks(StPicoDst const* co
 
     TArrayC Flag(tracksToUse.size());
     KFVertex aVertex;
-//    aVertex.ConstructPrimaryVertex((const KFParticle **) particles, tracksToUse.size(), (Bool_t*) Flag.GetArray(), TMath::Sqrt(StAnneling::Chi2Cut() / 2)); //original
-//    aVertex.SetConstructMethod(1);
-    aVertex.ConstructPrimaryVertex((const KFParticle **) particles, tracksToUse.size(), (Bool_t*) Flag.GetArray(), 3.5); //ok
+    aVertex.ConstructPrimaryVertex((const KFParticle **) particles, tracksToUse.size(), (Bool_t*) Flag.GetArray(), TMath::Sqrt(StAnneling::Chi2Cut() / 2)); //original
+//    aVertex.ConstructPrimaryVertex((const KFParticle **) particles, tracksToUse.size(), (Bool_t*) Flag.GetArray(), 3.5); //ok
 
     // clean up
     for(size_t iTrk = 0; iTrk < tracksToUse.size(); ++iTrk) delete particles[iTrk];
