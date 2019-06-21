928e0ab496a2cfb88a7b223b476d5063c9c31aeb
luicasko <l.kramarik@hotmail.sk>
2019-06-21 10:57:23 +0200

    refitVertex fucntion

--- StRoot/StPicoD0AnaMaker/StPicoD0AnaMaker.cxx
+++ StRoot/StPicoD0AnaMaker/StPicoD0AnaMaker.cxx
@@ -321,7 +321,7 @@ TVector3 StPicoD0AnaMaker::refitVertex(bool always){
     //removing according single track cuts
     if (singleTrack) {
         float dca;
-        for (unsigned short iTrack = 0; iTrack < nTracks; ++iTrack) {
+        for (unsigned short iTrack = 0; iTrack < mPicoDst->numberOfTracks(); ++iTrack) {
             StPicoTrack* trk = mPicoDst->track(iTrack);
             dca = (mPrimVtx - trk->origin()).Mag();
             if (dca>0.012 && trk->isPrimary()) tracksToRemove.push_back(iTrack);
