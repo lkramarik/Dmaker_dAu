975a4037447646da4080d3fbeede4e2af2fe0016
luicasko <l.kramarik@hotmail.sk>
2019-07-04 15:45:31 +0200

    hot spot histo

--- StRoot/StPicoD0AnaMaker/StPicoD0AnaMaker.cxx
+++ StRoot/StPicoD0AnaMaker/StPicoD0AnaMaker.cxx
@@ -79,7 +79,7 @@ int StPicoD0AnaMaker::InitHF() {
     mOutList->Add(new TH1D("h_tracktest_TOF","h_tracktest_TOF", 6, 0.5, 6.5));
 
     mOutFileBaseName = mOutFileBaseName.ReplaceAll(".root", "");
-    TString ntpVars = "grefMult:runId:eventId:primary:pi1_pt:pi1_dca:pi1_nSigma:pi1_nHitFit:pi1_TOFinvbeta:pi1_betaBase:k_pt:k_dca:k_nSigma:k_nHitFit:k_TOFinvbeta:k_betaBase:dcaDaughters:primVz:primVzVpd:D_theta:cosTheta:D_decayL:dcaD0ToPv:D_cosThetaStar:D_pt:D_mass";
+    TString ntpVars = "grefMult:runId:eventId:primary:diffRemovedPrimary:pi1_pt:pi1_dca:pi1_nSigma:pi1_nHitFit:pi1_TOFinvbeta:pi1_betaBase:k_pt:k_dca:k_nSigma:k_nHitFit:k_TOFinvbeta:k_betaBase:dcaDaughters:primVz:primVzVpd:D_theta:cosTheta:D_decayL:dcaD0ToPv:D_cosThetaStar:D_pt:D_mass";
 //    ntp_kaon = new TNtuple("ntp_kaon", "kaon tree","k_pt:k_phi:k_eta:k_nSigma:k_nHitFit:k_TOFinvbeta:pi_eventId:pi_runId");
 //    ntp_pion = new TNtuple("ntp_pion", "pion tree","pi_pt:pi_phi:pi_eta:pi_nSigma:pi_nHitFit:pi_TOFinvbeta:k_eventId:k_runId");
     ntp_DMeson_Signal = new TNtuple("ntp_signal","DMeson TreeSignal", ntpVars);
@@ -250,6 +250,7 @@ int StPicoD0AnaMaker::createCandidates() {
             ntVar[ii++] = mPicoEvent->runId();
             ntVar[ii++] = mPicoEvent->eventId();
             ntVar[ii++] = primary;
+            ntVar[ii++] = nPrimary-tracksToRemove.size();
 
             ntVar[ii++] = pion1->gPt();
             ntVar[ii++] = pair->particle1Dca();
