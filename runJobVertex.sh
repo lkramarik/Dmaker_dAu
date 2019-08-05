451344693557cf055ff5cbaf1309230e088e2cfd
luicasko <l.kramarik@hotmail.sk>
2019-08-05 22:24:28 +0200

    submit stufF

--- runJobVertex.sh
+++ runJobVertex.sh
@@ -12,7 +12,7 @@ cp ../../picoLists/runs_path_all.list  ./
 list="runs_path_all.list"
 
 #copy needed folders
-cp -r ../../.sl73_gcc485 ./
+cp -r ../../.sl* ./
 cp -Lr ../../StRoot ./
 cp ../../picoLists/picoList_bad.list ./
 mkdir starSubmit
