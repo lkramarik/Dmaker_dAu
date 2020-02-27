db557b9f9238decd7e9c5519deda8e29e66892d5
lukas <l.kramarik@hotmail.sk>
2020-02-27 09:07:56 +0100

    next

--- StRoot/macros/runSimInputsMaker.C
+++ StRoot/macros/runSimInputsMaker.C
@@ -16,9 +16,9 @@
 using namespace std;
 
 void runSimInputsMaker(
-    const char*  inputFile,
-    const Char_t *outputFile,  
-    const Char_t *badRunListFileName) {
+    const Char_t *inputFile="./picoLists/runs_local_test.list"
+    const Char_t *outputFile="outputLocal",
+    const Char_t *badRunListFileName = "./picoLists/picoList_bad.list") {
     string SL_version = "SL18f";
     string env_SL = getenv ("STAR");
     if (env_SL.find(SL_version)==string::npos) {
