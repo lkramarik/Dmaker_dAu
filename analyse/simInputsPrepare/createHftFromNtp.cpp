884b7b431788ca42c2bc267256499648aa81b35b
lukas <l.kramarik@hotmail.sk>
2020-05-14 01:04:59 +0200

    next

---
+++ analyse/simInputsPrepare/createHftFromNtp.cpp
@@ -0,0 +1,151 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include"TH1.h"
+#include"TH2.h"
+#include"TF1.h"
+#include"TCut.h"
+#include"TMath.h"
+#include"TCanvas.h"
+#include"TFile.h"
+#include"TLatex.h"
+#include"TStyle.h"
+#include"TNtuple.h"
+#include"TPad.h"
+#include"TLegend.h"
+#include"TPaveText.h"
+#include"TSystem.h"
+#include"TAxis.h"
+#include"TTree.h"
+#include"TFitResultPtr.h"
+#include"TFitResult.h"
+#include"TString.h"
+
+using namespace std;
+
+void draw(TH1D* histo, TString xAxis, TString yAxis, Int_t color) {
+    histo->GetXaxis()->SetTitle(xAxis);
+    histo->GetYaxis()->SetTitle(yAxis);
+    histo->SetLineColor(color);
+    histo->SetMarkerColor(color);
+    histo->SetStats(0);
+}
+
+void createHftFromNtp(){
+    Int_t colors[] = {1,46,8,4,5,6,7,9,41,42,44,45,16};
+//    TString fileNameData = "outputLocal.hists.root";
+//    TString fileNameData = "ntp.track.2403.root";
+//    TString fileNameData = "ntp.2403.sample.root";
+    TString fileNameData = "/home/lukas/work/dmesons/Dmaker_ndAu/Dmaker_dAu/analyse/simInputsPrepare/toHadd/ntp.ratio.1404.half.root";
+//    TString fileNameData = "/home/lukas/work/dmesons/Dmaker_ndAu/Dmaker_dAu/analyse/simInputsPrepare/toHadd/ntp.ratio.1404.half.rootprimary.root";
+    auto* dataF = new TFile(fileNameData,"r");
+    auto* dataOut = new TFile("ratio.root","recreate");
+    auto* ntpData = (TNtuple*)dataF -> Get("ntp_tracks");
+
+    Float_t dca, isHFT, zdc, isPrimary, nHitsFit, refMult, nHftTracks;
+
+    double maxPt = 6;
+
+    TH1D *pt_hist_HFT = new TH1D("pT_HFT", "pT_HFT", 100, 0.15, maxPt);
+    TH1D *pt_hist = new TH1D("pt_hist", "pt_hist", 100, 0.15, maxPt);
+
+//    ntp_tracks = new TNtuple("ntp_tracks","ntp_tracks", "pt:dca:isHFT:zdc:isPrimary:nHitsFit:refMult:nHftTracks");
+
+
+    const char* cuts[] = {"dca<=0.1", "dca<=1.5", "dca<=1.0", "dca<=0.7", "dca<=0.5", "dca<=0.3"};
+//    const char* cuts[] = {"dca<=1.5 && nHftTracks>1", "dca<=1.0 && nHftTracks>1", "dca<=0.7 && nHftTracks>1", "dca<=0.5 && nHftTracks>1", "dca<=0.3 && nHftTracks>1"};
+//    const char* cuts[] = {"dca<=1.5 && nHftTracks>0", "dca<=1.0 && nHftTracks>0", "dca<=0.7 && nHftTracks>0", "dca<=0.5 && nHftTracks>0", "dca<=0.3 && nHftTracks>0",
+//                          "dca<=1.5", "dca<=1.0", "dca<=0.7", "dca<=0.5", "dca<=0.3"};
+
+    TString namesSuffix[] = {"dca0.1_TofMatched", "dca1.5_TofMatched", "dca1.0_TofMatched", "dca0.7_TofMatched", "dca0.5_TofMatched", "dca0.3_TofMatched"};
+//    TString namesSuffix[] = {"dca1.5_TofMatched", "dca1.0_nHft1_TofMatched", "dca0.7_nHft1_TofMatched", "dca0.5_nHft1_TofMatched", "dca0.3_nHft1_TofMatched"};
+//    TString namesSuffix[] = {"dca1.5_nHft1", "dca1.0_nHft1", "dca0.7_nHft1", "dca0.5_nHft1", "dca0.3_nHft1"};
+
+//    TString namesSuffix[] = {"dca1.5_nHft0", "dca1.0_nHft0", "dca0.7_nHft0", "dca0.5_nHft0", "dca0.3_nHft0",
+//                             "dca1.5", "dca1.0", "dca0.7", "dca0.5", "dca0.3"};
+
+    TCut additionalCut = "isTOF>0 && isPrimary>0";
+
+    const int nBins = sizeof(namesSuffix) / sizeof(TString);
+
+    TH1D *pt_hist_clone[nBins];
+
+    TCanvas *c1 = new TCanvas("c1","c1",1200,900);
+    TCanvas *cwork = new TCanvas("cwork","cwork",1200,900);
+    TLegend *legend = new TLegend(0.155,0.789, 0.427, 0.884,"","brNDC");
+    for (int i = 0; i < 6; ++i) {
+        cout<<"nbin "<<i<<endl;
+        TCut cut = cuts[i];
+        cut+="pt>0.15";
+        cut+="pt<6";
+        cut+=additionalCut;
+        cout<<cut<<endl;
+        cwork->cd();
+        ntpData -> Project("pt_hist", "pt", cut);
+        cut+="isHFT>0";
+        ntpData -> Project("pT_HFT", "pt", cut);
+
+        pt_hist_clone[i] = (TH1D*)pt_hist_HFT->Clone(Form("HFT_matching_ratio_%i", i));
+        pt_hist_clone[i]->SetNameTitle(Form("HFT_matching_ratio_%s", namesSuffix[i].Data()), cuts[i]);
+        pt_hist_clone[i]->Sumw2();
+        pt_hist_clone[i]->Divide(pt_hist);
+        pt_hist_clone[i]->Write();
+        pt_hist_HFT->Write(Form("pt_HFT_%i_%s", i, namesSuffix[i].Data()));
+        pt_hist->Write(Form("pT_%i_%s", i, namesSuffix[i].Data()));
+        draw(pt_hist_clone[i], "p_{T} [GeV/c]", "HFT ratio from data", colors[i]);
+    }
+
+    cout<<"draw"<<endl;
+
+    for (int j = 0; j < 1; ++j) {
+        c1->cd();
+        if (j==0 )pt_hist_clone[j]->DrawCopy(); else pt_hist_clone[j]->DrawCopy("same");
+        legend->AddEntry(pt_hist_clone[j], cuts[j], "pl");
+    }
+
+    legend->Draw("same");
+
+    c1->SaveAs("hft/ratios.png");
+}
+
+
+void divide_ntp() {
+    TString input="/home/lukas/work/dmesons/Dmaker_ndAu/Dmaker_dAu/analyse/simInputsPrepare/toHadd/ntp.ratio.1404.half.root";
+    TFile* data = new TFile(input ,"r");
+    TFile *fileOut = new TFile(input+"primary.root", "RECREATE");  // output root file
+
+    TNtuple *ntpOut = new TNtuple("ntp_tracks","ntp_tracks", "pt:dca:isHFT:isTOF:zdc:nHitsFit:refMult:nHftTracks:nTofTracks:particleId");
+
+    TNtuple *ntp = (TNtuple*) data->Get("ntp_tracks");
+    Float_t  pt, dca, isHFT, isTOF, zdc, nHitsFit, refMult, nHftTracks, nTofTracks, particleId, isPrimary;
+
+    ntp->SetBranchAddress("pt", &pt);
+    ntp->SetBranchAddress("dca", &dca);
+    ntp->SetBranchAddress("isHFT", &isHFT);
+    ntp->SetBranchAddress("isTOF", &isTOF);
+    ntp->SetBranchAddress("zdc", &zdc);
+    ntp->SetBranchAddress("nHitsFit", &nHitsFit);
+    ntp->SetBranchAddress("refMult", &refMult);
+    ntp->SetBranchAddress("isPrimary", &isPrimary);
+    ntp->SetBranchAddress("nHftTracks", &nHftTracks);
+    ntp->SetBranchAddress("nTofTracks", &nTofTracks);
+    ntp->SetBranchAddress("particleId", &particleId);
+
+
+    const int nNtVars = ntpOut->GetNvar();
+    float ntVar[nNtVars];
+
+    cout<<"lets do this"<<endl;
+    for (long int i = 0; i < ntp->GetEntries(); i++) {
+        ntp->GetEntry(i);
+        if (isPrimary>0) {
+            ntpOut->Fill(pt,dca,isHFT,isTOF,zdc,nHitsFit,refMult,nHftTracks,nTofTracks,particleId);
+        }
+    }
+    fileOut->cd();
+    ntpOut->Write(ntpOut->GetName(), TObject::kOverwrite);
+    fileOut->Close();
+
+    data->Close();
+    cout<<"Done."<<endl;
+}
---
+++ analyse/simInputsPrepare/createQATracks.cxx
@@ -0,0 +1,383 @@
+#include"TSystem.h"
+#include"TAxis.h"
+#include"TTree.h"
+#include"TCanvas.h"
+#include"TFile.h"
+#include"TNtuple.h"
+#include"TH1.h"
+#include"TLegend.h"
+#include"TPad.h"
+#include"TFitResultPtr.h"
+#include"TFitResult.h"
+#include"TString.h"
+#include<iostream>
+#include<fstream>
+#include<vector>
+using namespace std;
+
+const int nDcaRatio=6;
+const float dcaCut[nDcaRatio]={0.3, 0.5, 0.7, 1.0, 1.2, 1.5};
+
+const int nPtDca=6;
+const float ptCut[nPtDca+1]={0.15, 0.3, 0.7, 1.2, 2, 3, 5};
+
+//_____________________________________________________________________________________________________________
+void draw(TH1D* histo, TString xAxis, TString yAxis, Int_t color) {
+    histo->GetXaxis()->SetTitle(xAxis);
+    histo->GetYaxis()->SetTitle(yAxis);
+    histo->SetLineColor(color);
+    histo->SetMarkerColor(color);
+    histo->SetMarkerStyle(20);
+    histo->SetStats(0);
+}
+
+//_____________________________________________________________________________________________________________
+void createQATracks() {
+    Int_t colors[] = {1,46,8,4,5,6,7,9,41,42,44,45,16};
+//    TString fileNameData = "outputLocal.hists.root";
+//    TString fileNameData = "ntp.track.2403.root";
+//    TString fileNameData = "ntp.2403.sample.root";
+    TString fileNameData = "/home/lukas/work/dmesons/Dmaker_ndAu/Dmaker_dAu/analyse/simInputsPrepare/toHadd/ntp.ratio.1404.half.rootprimary.root";
+//    TString fileNameData = "/home/lukas/work/dmesons/Dmaker_ndAu/Dmaker_dAu/analyse/simInputsPrepare/toHadd/ntp.ratio.1404.half.root";
+    auto* dataF = new TFile(fileNameData,"r");
+    auto* dataOut = new TFile("tracksQA.data.root","recreate");
+    auto* ntpData = (TNtuple*)dataF -> Get("ntp_tracks");
+
+    double maxPt = 6;
+
+    TString variables[] = {"dca", "nHitsFit"};
+    Double_t limMin[] = {0, 0.5};
+    Double_t limMax[] = {1.5, 50.5};
+    Int_t nBins[] = {100, 51};
+    TH1D *his;
+    TH1D *hisHft;
+
+    Float_t  pt, dca, isHFT, isTOF, zdc, nHitsFit, refMult, nHftTracks, nTofTracks, particleId, isPrimary;
+    ntpData->SetBranchAddress("pt", &pt);
+    ntpData->SetBranchAddress("dca", &dca);
+    ntpData->SetBranchAddress("isHFT", &isHFT);
+    ntpData->SetBranchAddress("isTOF", &isTOF);
+    ntpData->SetBranchAddress("zdc", &zdc);
+    ntpData->SetBranchAddress("nHitsFit", &nHitsFit);
+    ntpData->SetBranchAddress("refMult", &refMult);
+//    ntpData->SetBranchAddress("isPrimary", &isPrimary);
+    ntpData->SetBranchAddress("nHftTracks", &nHftTracks);
+    ntpData->SetBranchAddress("nTofTracks", &nTofTracks);
+    ntpData->SetBranchAddress("particleId", &particleId);
+
+    Double_t maxDca=0.2;
+
+    //init histos
+    TString histoname;
+    TH1D* hnHitsFit = new TH1D("hnHitsFit", "hnHitsFit", 51, 0.5, 50.5);
+    TH1D* hnHitsFitHft = new TH1D("hnHitsFitHft", "hnHitsFitHft", 51, 0.5, 50.5);
+    TH1D* hDcaAll = new TH1D("hDca_all", "hDca_all", 100, 0, maxDca);
+    TH1D* hDcaAllHft = new TH1D("hDca_all_hft", "hDca_all_hft", 100, 0, maxDca);
+
+
+    TH1D* hPtRecoDCA[10];
+    TH1D* hPtRecoHftDCA[10];
+    TH1D* hDCAPtBins[10];
+    TH1D* hDCAPtBinsHft[10];
+
+    for (int j = 0; j < nDcaRatio; ++j) {
+        histoname=Form("hPt_dca%.1f", dcaCut[j]);
+        hPtRecoDCA[j]=new TH1D(histoname, histoname, 100,0,10);
+
+        histoname=Form("hPt_hft_dca%.1f", dcaCut[j]);
+        hPtRecoHftDCA[j]=new TH1D(histoname, histoname, 100,0,10);
+    }
+
+    for (int j = 0; j < nPtDca; ++j) {
+        histoname=Form("hDca_pt_%.2f_%.2f", ptCut[j], ptCut[j+1]);
+        hDCAPtBins[j]=new TH1D(histoname, histoname, 100, 0, maxDca);
+
+        histoname=Form("hDca_hft_pt_%.2f_%.2f", ptCut[j], ptCut[j+1]);
+        hDCAPtBinsHft[j]=new TH1D(histoname, histoname, 100, 0, maxDca);
+    }
+
+    const int nVars = sizeof(variables) / sizeof(TString);
+    for (int k = 0; k < ntpData->GetEntries(); ++k) {
+        ntpData->GetEntry(k);
+        if (isTOF==0) continue;
+        if (dca>1.5) continue;
+
+        hnHitsFit->Fill(nHitsFit);
+        hDcaAll->Fill(dca);
+
+        for (int j = 0; j < nPtDca; ++j) {
+            if (pt>ptCut[j] && pt<ptCut[j+1]) hDCAPtBins[j]->Fill(dca);
+        }
+        for (int i = 0; i < nDcaRatio; ++i) {
+            if (dca<dcaCut[i]) hPtRecoDCA[i]->Fill(pt);
+        }
+
+        if (isHFT>0) {
+            hnHitsFitHft->Fill(nHitsFit);
+            hDcaAllHft->Fill(dca);
+            for (int j = 0; j < nPtDca; ++j) {
+                if (pt>ptCut[j] && pt<ptCut[j+1]) hDCAPtBinsHft[j]->Fill(dca);
+            }
+            for (int i = 0; i < nDcaRatio; ++i) {
+                if (dca<dcaCut[i]) hPtRecoHftDCA[i]->Fill(pt);
+            }
+        }
+
+//        for (int i = 0; i < nVars; ++i) {
+//            cout<<"working on "<<variables[i]<<endl;
+//            his = new TH1D("hist", variables[i], nBins[i], limMin[i], limMax[i]);
+//            hisHft = new TH1D("hist_HFT", variables[i], nBins[i], limMin[i], limMax[i]);
+//            ntpData -> Project("hist", variables[i]);
+//            ntpData -> Project("hist_HFT", variables[i], "isHFT>0");
+//
+//            his->Scale(1/his->GetEntries());
+//            hisHft->Scale(1/hisHft->GetEntries());
+//
+//            dataOut->cd();
+//            hisname = Form("%s", variables[i].Data());
+//            his->Write(hisname);
+//            hisname = Form("%s_hft", variables[i].Data());
+//            hisHft->Write(hisname);
+//        }
+    }
+    TList *listOutQA = new TList();
+    listOutQA->Add(hnHitsFit);
+    listOutQA->Add(hnHitsFitHft);
+    listOutQA->Add(hDcaAll);
+    listOutQA->Add(hDcaAllHft);
+    for (int l = 0; l < nPtDca; ++l) {
+        listOutQA->Add(hDCAPtBins[l]);
+        listOutQA->Add(hDCAPtBinsHft[l]);
+    }
+
+    for (int j = 0; j < nDcaRatio; ++j) {
+        listOutQA->Add(hPtRecoDCA[j]);
+        listOutQA->Add(hPtRecoHftDCA[j]);
+    }
+
+    dataOut->cd();
+    listOutQA->Write("hists_QA", 1, 0);
+
+    dataF->Close();
+    dataOut->Close();
+}
+
+//________________________________________________________________________________________________________________________
+void compareEmb() {
+    TString fileNameData = "/home/lukas/work/dmesons/Dmaker_ndAu/Dmaker_dAu/analyse/simInputsPrepare/tracksQA.data.root";
+    TString fileNameSim = "/home/lukas/work/D0-fullEvent/analyse/test.out.root";
+
+    auto* dataF = new TFile(fileNameData,"r");
+    auto* dataSimF = new TFile(fileNameSim,"r");
+
+
+    TList *list[2];
+    list[0] = (TList*)dataF -> Get("hists_QA;1");
+    list[1] = (TList*)dataSimF -> Get("hists_QA;1");
+
+    TString names[] = {"data", "sim"};
+
+    Int_t color[]={46,9};
+
+    TString histoname;
+
+    TH1D* hnHitsFit[2];
+    TH1D* hnHitsFitHft[2];
+    TH1D* hDcaAll[2];
+    TH1D* hDcaAllHft[2];
+    TH1D* hPtRecoDCA[2][10];
+    TH1D* hPtRecoHftDCA[2][10];
+    TH1D* hDCAPtBins[2][10];
+    TH1D* hDCAPtBinsHft[2][10];
+
+    for (int k = 0; k < 2; ++k) {
+        cout<<names[k]<<endl;
+        hDcaAll[k]=new TH1D();
+        hDcaAll[k]=static_cast<TH1D*>(list[k]->FindObject("hDca_all"));
+        hDcaAll[k]->SetTitle("");
+        draw(hDcaAll[k], "DCA [cm]", "1/N_{entries}", color[k]);
+
+        hDcaAllHft[k]=new TH1D();
+        hDcaAllHft[k]=static_cast<TH1D*>(list[k]->FindObject("hDca_all_hft"));
+        hDcaAllHft[k]->SetTitle("HFT");
+        draw(hDcaAllHft[k], "DCA [cm]", "1/N_{entries}", color[k]);
+
+        hnHitsFit[k]=new TH1D();
+        hnHitsFit[k]=static_cast<TH1D*>(list[k]->FindObject("hnHitsFit"));
+        hnHitsFit[k]->SetTitle("");
+        draw(hnHitsFit[k], "nHitsFit", "1/N_{entries}", color[k]);
+
+        hnHitsFitHft[k]=new TH1D();
+        hnHitsFitHft[k]=static_cast<TH1D*>(list[k]->FindObject("hnHitsFitHft"));
+        hnHitsFitHft[k]->SetTitle("HFT");
+        draw(hnHitsFitHft[k], "nHitsFit", "1/N_{entries}", color[k]);
+
+        for (int j = 0; j < nDcaRatio; ++j) {
+            histoname=Form("hPt_dca%.1f", dcaCut[j]);
+            hPtRecoDCA[k][j]=new TH1D();
+            hPtRecoDCA[k][j]=static_cast<TH1D*>(list[k]->FindObject(histoname));
+            histoname=Form("DCA < %.1f cm", dcaCut[j]);
+            hPtRecoDCA[k][j]->SetTitle(histoname);
+            draw(hPtRecoDCA[k][j],"p_{T} [GeV/c]", "1/N_{entries}", color[k]);
+
+            histoname=Form("hPt_hft_dca%.1f", dcaCut[j]);
+            hPtRecoHftDCA[k][j]=new TH1D();
+            hPtRecoHftDCA[k][j]=static_cast<TH1D*>(list[k]->FindObject(histoname));
+            histoname=Form("HFT, DCA < %.1f cm", dcaCut[j]);
+            hPtRecoHftDCA[k][j]->SetTitle(histoname);
+            draw(hPtRecoHftDCA[k][j], "p_{T} [GeV/c]", "1/N_{entries}", color[k]);
+
+        }
+
+        for (int j = 0; j < nPtDca; ++j) {
+            histoname=Form("hDca_pt_%.2f_%.2f", ptCut[j], ptCut[j+1]);
+            hDCAPtBins[k][j]=new TH1D();
+            hDCAPtBins[k][j]=static_cast<TH1D*>(list[k]->FindObject(histoname));
+            histoname=Form("%.2f < p_{T} < %.2f GeV/c", ptCut[j], ptCut[j+1]);
+            hDCAPtBins[k][j]->SetTitle(histoname);
+            draw(hDCAPtBins[k][j], "DCA [cm]", "1/N_{entries}", color[k]);
+
+            histoname=Form("hDca_hft_pt_%.2f_%.2f", ptCut[j], ptCut[j+1]);
+            hDCAPtBinsHft[k][j]=new TH1D();
+            hDCAPtBinsHft[k][j]=static_cast<TH1D*>(list[k]->FindObject(histoname));
+            histoname=Form("HFT, %.2f < p_{T} < %.2f GeV/c", ptCut[j], ptCut[j+1]);
+            hDCAPtBinsHft[k][j]->SetTitle(histoname);
+            draw(hDCAPtBinsHft[k][j], "DCA [cm]", "1/N_{entries}", color[k]);
+
+        }
+
+    }
+
+    TLegend *legend = new TLegend(0.767, 0.78, 0.92, 0.89);
+    legend -> SetFillStyle(0);
+    legend -> SetLineColor(0);
+    legend -> SetTextSize(0.04);
+    legend -> AddEntry(hDCAPtBins[0][0], names[0], "pl");
+    legend -> AddEntry(hDCAPtBins[1][0], names[1], "pl");
+
+    const int nCanvas = nDcaRatio+nPtDca+2;
+    cout<<nCanvas<<endl;
+
+    TCanvas* c[nCanvas];
+    for (int i = 0; i < nCanvas; ++i) {
+        histoname=Form("c%i", i);
+        c[i]=new TCanvas(histoname, histoname, 1500,800);
+        c[i]->Divide(2,1);
+    }
+
+    int canId = -1;
+
+    canId++;
+    for (int k = 0; k < 2; ++k) {
+        c[canId]->cd(1);
+        gPad->SetLogy();
+        gPad->SetLeftMargin(0.15);
+        gPad->SetRightMargin(0.05);
+
+        hnHitsFit[k]->Scale(1/hnHitsFit[k]->GetEntries());
+        if (k==0) hnHitsFit[k]->Draw();
+        hnHitsFit[k]->Draw("same");
+    }
+
+    for (int k = 0; k < 2; ++k) {
+        c[canId]->cd(2);
+        gPad->SetLogy();
+        gPad->SetLeftMargin(0.15);
+        gPad->SetRightMargin(0.05);
+
+        hnHitsFitHft[k]->Scale(1/hnHitsFitHft[k]->GetEntries());
+        if (k==0) hnHitsFitHft[k]->Draw();
+        hnHitsFitHft[k]->Draw("same");
+        legend->Draw("same");
+    }
+    //______________________________________________________
+    canId++;
+    for (int k = 0; k < 2; ++k) {
+        c[canId]->cd(1);
+        gPad->SetLogy();
+        gPad->SetLeftMargin(0.15);
+        gPad->SetRightMargin(0.05);
+
+        hDcaAll[k]->Scale(1/hDcaAll[k]->GetEntries());
+        if (k==0) hDcaAll[k]->Draw();
+        hDcaAll[k]->Draw("same");
+    }
+
+    for (int k = 0; k < 2; ++k) {
+        c[canId]->cd(2);
+        gPad->SetLogy();
+        gPad->SetLeftMargin(0.15);
+        gPad->SetRightMargin(0.05);
+
+        hDcaAllHft[k]->Scale(1/hDcaAllHft[k]->GetEntries());
+        if (k==0) hDcaAllHft[k]->Draw();
+        hDcaAllHft[k]->Draw("same");
+        legend->Draw("same");
+    }
+    //______________________________________________________
+    for (int j = 0; j < nDcaRatio; ++j) {
+        canId++;
+        for (int k = 0; k < 2; ++k) {
+            c[canId]->cd(1);
+            gPad->SetLogy();
+            gPad->SetLeftMargin(0.15);
+            gPad->SetRightMargin(0.05);
+
+            hPtRecoDCA[k][j]->Scale(1/hPtRecoDCA[k][j]->GetEntries());
+            if (k==0) hPtRecoDCA[k][j]->Draw();
+            hPtRecoDCA[k][j]->Draw("same");
+        }
+
+        for (int k = 0; k < 2; ++k) {
+            c[canId]->cd(2);
+            gPad->SetLogy();
+            gPad->SetLeftMargin(0.15);
+            gPad->SetRightMargin(0.05);
+
+            hPtRecoHftDCA[k][j]->Scale(1/hPtRecoHftDCA[k][j]->GetEntries());
+            if (k==0) hPtRecoHftDCA[k][j]->Draw();
+            hPtRecoHftDCA[k][j]->Draw("same");
+            legend->Draw("same");
+
+        }
+    }
+
+    for (int j = 0; j < nPtDca; ++j) {
+        canId++;
+        for (int k = 0; k < 2; ++k) {
+            c[canId]->cd(1);
+            gPad->SetLogy();
+            gPad->SetLeftMargin(0.15);
+            gPad->SetRightMargin(0.05);
+
+            hDCAPtBins[k][j]->Scale(1/hDCAPtBins[k][j]->GetEntries());
+            if (k==0) hDCAPtBins[k][j]->Draw();
+            hDCAPtBins[k][j]->Draw("same");
+        }
+
+        for (int k = 0; k < 2; ++k) {
+            c[canId]->cd(2);
+            gPad->SetLogy();
+            gPad->SetLeftMargin(0.15);
+            gPad->SetRightMargin(0.05);
+
+            hDCAPtBinsHft[k][j]->Scale(1/hDCAPtBinsHft[k][j]->GetEntries());
+            if (k==0) hDCAPtBinsHft[k][j]->Draw();
+            hDCAPtBinsHft[k][j]->Draw("same");
+            legend->Draw("same");
+
+        }
+
+    }
+
+    c[0]->SaveAs("trackQA.pdf(");
+    for (int i = 1; i < nCanvas-2; ++i) {
+        c[i]->SaveAs("trackQA.pdf");
+    }
+    c[nCanvas-1]->SaveAs("trackQA.pdf)");
+
+    for (int i = 0; i < nCanvas; ++i) {
+        c[i]->Close();
+    }
+
+}
